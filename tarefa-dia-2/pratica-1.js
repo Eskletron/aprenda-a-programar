daf80529735b2074484f306ce4ae5dfe9a5dfb7e
Ruan <bhruncosta@gmail.com>
2022-08-24 16:50:19 -0300

    exibindo os valores das variaveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email = "bhruncosta@gmail.com"
 let profissão = "programador"
 let idade = 29
 let destino = "japan"
+console.log(Nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(destino)
