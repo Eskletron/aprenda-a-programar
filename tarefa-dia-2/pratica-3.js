3e3e20cdd82307e3fc90e4d3841ecfbb6f409f86
Ruan <bhruncosta@gmail.com>
2022-08-25 17:06:35 -0300

    Verificando o tamanho da string com o comango lenght

---
+++ tarefa-dia-2/pratica-3.js
@@ -0,0 +1,6 @@
+let codigo = "codigo imensamente grande que talvez possibilite a derrota dos nazista caso os programadores do Ituring consigam decifrar esse tal codigo imensamente grande que alguem sabe la de onde fez podendo facilitar udo e botar numa cartinha ou algo assim era como se não quisesse vencer a guerra contra os nazista tudo navida e questao de querer ou talvez nem tudo mas e um guerra meu irmao"
+let tamanho = codigo.length
+console.log(tamanho)
+if(tamanho >= 6110) { console.log( "Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. ; ' (")}
+ else { console.log( "Maravilha! O código tem menos ou é igual a 6111 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocÊs vão conseguir derrotá-los! :D")}
+ 
