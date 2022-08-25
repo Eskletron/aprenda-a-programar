e695fa7b8675e906558ea94b32a2a365f88b331d
Ruan <bhruncosta@gmail.com>
2022-08-25 15:04:43 -0300

    subindo o desafio para o repositorio remoto

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,3 +8,5 @@ console.log(email)
 console.log(profissão)
 console.log(idade)
 console.log(destino)
+let desafio = "Ola, " + Nome + " ! Somos aqui da agencia de turismo Viagem dos Sonhos. Estamos te escrevendo este eemail, pois acabamos de confrimar as compras das passagens aereas para " +  destino + " . Confirma pra gente alguns dados? A sua iidade é " + idade + ", você é " + profissão + " e o seu email é " + email + " ? Ficamos no aguardo. Muito obrigado e boa vaigem! :D"
+console.log(desafio)
